3ff0cffcdbede9bd710a54a8ed83b4e586261f85
tamusandesh99 <tamusandesh99@gmail.com>
2023-07-21 21:23:36 -0400

    added user display

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const UserProfile = () => {
+  const username = useSelector(state => state.profile.username);
   return (
     <>
     <div className=''>userprofile</div>
+    <div>Username: {username}</div> {/* Display the username */}
     </>
   )
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,5 +4,5 @@ import { combineReducers } from 'redux'
 
 export default combineReducers({
     auth,
-    // profile
+    profile
 })
