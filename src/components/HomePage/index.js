f74251caa616b997b9ce9d4c946f81e9711a9d0e
tamusandesh99 <tamusandesh99@gmail.com>
2023-08-24 19:08:54 -0400

    added left and right content on homepage

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,13 +1,11 @@
-import React, {Fragment, useState, useEffect} from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { get_top_posts } from "../../actions/posts";
 import { connect } from "react-redux";
 import "./index.scss";
 
-
 const HomePage = (isAuthenticated) => {
-
   const [topPosts, setTopPosts] = useState([]);
   useEffect(() => {
     // Call the load_posts function and handle the data
@@ -22,7 +20,6 @@ const HomePage = (isAuthenticated) => {
       });
   }, []);
 
-
   let navigate = useNavigate();
 
   const handleButtonClick = (webLink, username, title, tech_stack) => {
@@ -30,26 +27,22 @@ const HomePage = (isAuthenticated) => {
       state: {
         username: username,
         title: title,
-        tech_stack: tech_stack
-      }
-      });
+        tech_stack: tech_stack,
+      },
+    });
   };
 
   const loginLink = (
     <Fragment>
       <div className="nav-item">
-        <Link  to="/login">
-          Login to make a post
-        </Link>
+        <Link to="/login">Login to make a post</Link>
       </div>
     </Fragment>
   );
   const postLink = (
     <Fragment>
       <div className="nav-item">
-        <Link to="/submitpost">
-          Submit Post
-        </Link>
+        <Link to="/submitpost">Submit Post</Link>
       </div>
     </Fragment>
   );
@@ -57,42 +50,55 @@ const HomePage = (isAuthenticated) => {
   return (
     <>
       <div className="main-page">
-        <p>Rate my website</p>
-        {topPosts.map((post , index) => (
-        <div className="single-post" key={index}>
-          <div className="post-info">
-            <p className="post-username">{post.username}</p>
-            <p className="post-title">{post.title}</p>
-            <p className="post-tech-stack">{post.tech_stack}</p>
+        <div className="left-homepage">this is left</div>
+        <div className="center-homepage">
+          <p>Rate my website</p>
+          {topPosts.map((post, index) => (
+            <div className="single-post" key={index}>
+              <div className="post-info">
+                <p className="post-username">{post.username}</p>
+                <p className="post-title">{post.title}</p>
+                <p className="post-tech-stack">{post.tech_stack}</p>
+              </div>
+              <div className="website-preview">
+                <button
+                  onClick={() =>
+                    handleButtonClick(
+                      post.website_link,
+                      post.username,
+                      post.title,
+                      post.tech_stack
+                    )
+                  }
+                >
+                  IMG
+                </button>
+              </div>
+            </div>
+          ))}
+          <div className="load-all-websites">
+            <ul className="load-websites">
+              <div>
+                <Link to="/posts">see all posts</Link>
+              </div>
+            </ul>
+            <ul className="load-websites">
+              <div>
+                {isAuthenticated.isAuthenticated ? postLink : loginLink}
+              </div>
+            </ul>
           </div>
-          <div className="website-preview">
-              <button onClick={() => handleButtonClick(post.website_link, post.username, post.title, post.tech_stack)}>
-              IMG
-              </button>
-            </div>
         </div>
-      ))}
-        <div className="load-all-websites">
-          <ul className="load-websites">
-            <div>
-            <Link to="/posts" >see all posts</Link>
-            </div>
-          </ul>
-          <ul className="load-websites">
-            <div>
-            {isAuthenticated.isAuthenticated ? postLink : loginLink}
-            </div>
-          </ul>
-          
-        </div>
+        <div className="right-homepage">this is left</div>
       </div>
+
+
     </>
   );
 };
 
-const mapStateToProps = state =>({
-  isAuthenticated: state.auth.isAuthenticated
-})
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
 
-
-export default connect(mapStateToProps) (HomePage);
+export default connect(mapStateToProps)(HomePage);
