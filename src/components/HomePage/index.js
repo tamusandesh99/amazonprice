0373b1fe47f757de0c15d691ac1c77e0b490bed5
tamusandesh99 <tamusandesh99@gmail.com>
2023-09-06 22:16:30 -0400

    aligned homepage with navbar

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -154,6 +154,9 @@ const HomePage = (isAuthenticated) => {
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
             ligula massa, varius a, semper congue, euismod non, mi.
           </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisc, varius a, semper congue, euismod non, mi.
+          </p>
         </div>
       </div>
     </>
