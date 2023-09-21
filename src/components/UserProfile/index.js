335d8a1d50733efa9a3c7d1ed7b2403d5f15d65a
tamusandesh99 <tamusandesh99@gmail.com>
2023-09-21 16:07:48 -0400

    added user dashboard

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { connect } from "react-redux";
 import { update_profile } from "../../actions/profile";
@@ -14,32 +15,81 @@ const UserProfile = ({ username, user_posts_global }) => {
 
   const { user_posts } = userData;
 
-
-
   useEffect(() => {
     setUserData({
-      user_posts: user_posts_global
-    })
-  },[user_posts_global])
+      user_posts: user_posts_global,
+    });
+  }, [user_posts_global]);
+
+
+  let navigate = useNavigate();
+  const handleButtonClick = (webLink, username, title, tech_stack) => {
+    navigate(`/posts/${encodeURIComponent(webLink)}`, {
+      state: {
+        username: username,
+        title: title,
+        tech_stack: tech_stack,
+      },
+    });
+  };
 
   return (
     <>
-      <div className="user_dashboard">
-        <div>Username: {username}</div>
-          <div className="form-group">
-             <ul>
-          {user_posts.map((post, index) => (
-            <li key={index}>
-              Title: {post.title}
-              <br />
-              Website Link: {post.website_link}
-              <br />
-              Tech Stack: {post.tech_stack}
-              <br />
-            </li>
+      <div className="user-dashboard">
+        {/* <div>Username: {username}</div>
+        <div className="form-group">
+          <ul>
+            {user_posts.map((post, index) => (
+              <li key={index}>
+                Title: {post.title}
+                <br />
+                Website Link: {post.website_link}
+                <br />
+                Tech Stack: {post.tech_stack}
+                <br />
+              </li>
+            ))}
+          </ul>
+        </div> */}
+
+        {user_posts.map((post, index) => (
+            <div
+              className="single-post"
+              onClick={() =>
+                handleButtonClick(
+                  post.website_link,
+                  post.username,
+                  post.title,
+                  post.tech_stack
+                )
+              }
+              key={index}
+            >
+              <div className="post-info">
+                <p className="post-username">{post.username}</p>
+                <p className="post-title">{post.title}</p>
+                <p className="post-tech-stack">{post.tech_stack}</p>
+              </div>
+              <div className="website-preview">
+                <button
+                  className="preview-button"
+                  onClick={() =>
+                    handleButtonClick(
+                      post.website_link,
+                      post.username,
+                      post.title,
+                      post.tech_stack
+                    )
+                  }
+                >
+                  <img
+                    src={post.website_link + '/favicon.ico'}
+                    alt={post.title}
+                  />
+                </button>
+              </div>
+            </div>
           ))}
-        </ul>
-          </div>
       </div>
     </>
   );
@@ -50,4 +100,4 @@ const mapStateToProps = (state) => ({
   user_posts_global: state.profile.user_posts,
 });
 
-export default connect(mapStateToProps,)(UserProfile);
+export default connect(mapStateToProps)(UserProfile);
