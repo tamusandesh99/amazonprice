814f5cabb71b1ea86529a232192a91824b9c01a2
tamusandesh99 <tamusandesh99@gmail.com>
2023-10-09 22:39:38 -0400

    fix: style on login

--- src/components/UserLogin/index.js
+++ src/components/UserLogin/index.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { login } from "../../actions/auth";
-import {ImUser} from 'react-icons/im'
-import {HiMiniLockClosed} from 'react-icons/hi2'
+import { ImUser } from "react-icons/im";
+import { HiMiniLockClosed } from "react-icons/hi2";
 import CSRFToken from "../CSRFToken";
 import "./index.scss";
 import { useNavigate, Link } from "react-router-dom";
@@ -29,43 +29,45 @@ const UserLogin = ({ login, isAuthenticated }) => {
       navigate("/"); // Redirect to home page after successful login
     }
   }, [isAuthenticated, navigate]);
-  
+
   return (
     <>
-      <form className="login-form" onSubmit={handleLogin}>
-        <CSRFToken />
-        <div className="form-group">Login</div>
-        <div className="form-group">
-        <ImUser className="user-icon"></ImUser>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={onChange}
-            required
-            placeholder="Enter you username"
-          />
-        </div>
-        <div className="form-group">
-          <HiMiniLockClosed></HiMiniLockClosed>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={onChange}
-            required
-            placeholder="Password"
-          />
-        </div>
-        <div className="form-group">
-          <button type="submit">Login</button>
-        </div>
-      </form>
-      <p>
+      <div className="form-container">
+        <form className="login-form" onSubmit={handleLogin}>
+          <CSRFToken />
+          <div className="form-title">Login</div>
+          <div className="form-group">
+            <ImUser className="form-icon"></ImUser>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={onChange}
+              required
+              placeholder="Enter you username"
+            />
+          </div>
+          <div className="form-group">
+            <HiMiniLockClosed className="form-icon"></HiMiniLockClosed>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              required
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-button">
+            <button type="submit">Login</button>
+          </div>
+        </form>
+        <p>
           Don't have an account? <Link to="/register">Signup</Link>
         </p>
+      </div>
     </>
   );
 };
